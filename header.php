a8cd89fcce6747085abe584a36532ea1aeb9b06a
vbloke <vbloke@gmail.com>
2014-11-12 09:05:43 +0000

    header meta

--- header.php
+++ header.php
@@ -4,10 +4,12 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="DC.title" content="Prydonian Digital" />
+<meta name="ICBM" content="51.4779645, 0.0142191" />
+<meta name="geo.position" content="51.4779645, 0.0142191" />	
 <title><?php wp_title('|', true, 'right'); ?><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
 <?php wp_head(); ?>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
-
 </head>
 <body <?php body_class(); ?>>
 
